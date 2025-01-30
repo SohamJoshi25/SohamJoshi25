f2e41be9a6628ee1757305fd638122d45b69bbaa
Soham Joshi <sohamjoshichinchwad@gmail.com>
2025-01-30 12:34:30 +0530

    contact email update

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,5 @@
 export const Breakpoint = 660;
-export const Email = "sohamjoshi.official@gmail.com";
+export const Email = "contact@sohamjoshi.in";
 export const Github = "https://github.com/SohamJoshi25";
 export const Linkedin = "https://www.linkedin.com/in/sohamjoshi25/utm_source=share&utm_campaign=portfolio&utm_content=profile";
 export const Resume= "https://drive.google.com/file/d/1fN0e8kDn4AXHlTk3S_nkP1V27gWg3cP6/view?usp=sharing";
