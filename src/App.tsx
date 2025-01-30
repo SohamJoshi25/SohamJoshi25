29391177f9d74789d68afc83219604b109cc925d
Soham Joshi <sohamjoshichinchwad@gmail.com>
2025-01-30 12:17:59 +0530

    Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //Pages
 import Home from './pages/home/Home.tsx'
+import PageNotFound from './pages/PageNotFound/NotFound.tsx';
 
 const App = () => {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/*" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
   )
---
+++ src/pages/PageNotFound/NotFound.tsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
+const PageNotFound = () => {
+  const navigate = useNavigate()
+  return (
+    <div className="flex justify-center items-center h-dvh">
+      <div className="flex justify-center flex-col items-center gap-4 ">
+        <span className="font-sans text-9xl">404</span>
+        <span>Page Not Found</span>
+        <button className=" p-2 px-3 rounded-md font-semibold hover:bg-slate-100 mt-10" onClick={()=>navigate("/")}>← Back to Home</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default PageNotFound
