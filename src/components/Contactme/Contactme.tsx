feae6bcd0a8956b0ed3f1f80925a928686ead679
Soham Joshi <sohamjoshichinchwad@gmail.com>
2025-02-20 23:50:22 +0530

    contact me

--- src/components/Contactme/Contactme.tsx
+++ src/components/Contactme/Contactme.tsx
@@ -42,23 +42,23 @@ const Contactme : React.FC<ContactType> = ({forwardRef}) => {
     }
 
   return (
-    <div className="w-full flex justify-center flex-col items-center pt-[100px] pb-[70px]  bg-gradient-to-r from-[hsl(0,0%,90%)] via-50% via-[hsl(0,0%,89%)] to-[hsl(0,0%,88%)]" ref={forwardRef}>
+    <div className="w-full flex justify-center flex-col items-center pt-[100px] pb-[70px] px-2 bg-gradient-to-r from-[hsl(0,0%,90%)] via-50% via-[hsl(0,0%,89%)] to-[hsl(0,0%,88%)]" ref={forwardRef}>
 
-      <div className="mb-[90px] text-gradiant m-auto w-full text-center animate-appear-title">
-        <span className="sm:text-[62px] text-[42px] noto-sans-450 " style={{"fontWeight":"500"}}>CONTACT ME</span>
+      <div className="mb-[90px] px-10 text-gradiant m-auto w-full text-center animate-appear-title">
+        <span className="lg:text-[62px] text-[42px] noto-sans-450 " style={{"fontWeight":"500"}}>CONTACT ME</span>
       </div> 
 
       <div className="flex items-start xl:flex-row flex-col justify-center gap-y-[80px] gap-x-[30px] xl:max-w-[1450px] w-[70%] xl:w-[74%] sm:min-w-[420px] h-fit ">
 
         <div className="w-full flex flex-col justify-center items-center text-center xl:text-left xl:justify-end items-top">
-            <h1 className="sm:text-[32px] text-[28px] font-[400] max-w-[400px]"><strong>Thank You </strong>For Visiting My Portfolio Website</h1>
-            <h2 className="sm:text-[24px] text-[20px] tracking-tight mt-[50px] font-[300] max-w-[400px]">I’d love to hear from you! Whether it’s a project idea, collaboration, or just to say hi – feel free to drop a message.</h2>
+            <h1 className="sm:text-[32px] text-[28px] font-[400] max-w-[400px] px-5"><strong>Thank You </strong>For Visiting My Portfolio Website</h1>
+            <h2 className="sm:text-[24px] text-[20px] tracking-tight mt-[50px] font-[300] max-w-[400px] px-5">I’d love to hear from you! Whether it’s a project idea, collaboration, or just to say hi – feel free to drop a message.</h2>
         </div>
 
-        <div className="xl:max-w-[1450px] xl:w-[100%] w-full m-auto p-[3px] pb-[10px]" >
+        <div className="xl:max-w-[1450px] xl:w-[100%] w-[80%] m-auto py-[3px] pb-[10px] " >
 
             <div className="">
-                <div className="sm:text-[16px] text-[14px] tracking-wider mb-[2px]">YOUR NAME</div>
+                <div className="md:text-[14px] text-[16px] tracking-wider mb-[2px]">YOUR NAME</div>
                 <input type="text" value={name} className="block w-full text-sm h-[44px] p-[10px] outline-none rounded-[4px] shadow-sm" placeholder="Your Name" onChange={(e)=>{setName(e.target.value)}}/>
             </div>
 
