653e7b823ca8c9ec560547bb04e21b5b54b878ff
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-04-16 22:50:45 +0530

    Added Resume Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 // Packages
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 //Pages
 import Home from './pages/home/Home.tsx'
 import PageNotFound from './pages/PageNotFound/NotFound.tsx';
+import { Resume } from './data/constants.ts';
 
 const App = () => {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Navigate to={Resume}/>} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
