b5cfd65691dd7d7ab9144103eb3b70fa3669b63e
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-04-16 23:02:58 +0530

    Added Resume Route with replace

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/home/Home.tsx'
 import PageNotFound from './pages/PageNotFound/NotFound.tsx';
 import { Resume } from './data/constants.ts';
+import { useEffect } from 'react';
 
 const App = () => {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<Navigate to={Resume} replace={true}/>} />
+          <Route path="/resume" element={(() => {useEffect(() => {window.location.replace(Resume);}, []);return null;})()} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
