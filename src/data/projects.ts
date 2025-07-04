37b43dbf2e04b92ca9f05fec340b7eefc4166834
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-07-04 10:57:41 +0530

    Project Link Update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,7 +12,7 @@ export const ProjectsData : ProjectType[] = [
         title:"Drive Connect",
         isFinished:true,
         text:"DriveConnect is a project that seamlessly integrates multiple Google Drive accounts, providing users with a unified storage experience. It aims to minimize internal fragmentation that arises when managing multiple Google Drives separately. The motivation behind this project stems from the common challenges users face in their daily lives while using Google Drive. By connecting multiple Google Drive accounts, DriveConnect offers a single, consolidated storage space, making file management more efficient and hassle-free.",
-        onClickLink:"https://driveconnect-app.vercel.app/"
+        onClickLink:"https://driveconnect.sohamjoshi.in"
     },
     {
         image:"/Projects/dnsserver.png",
