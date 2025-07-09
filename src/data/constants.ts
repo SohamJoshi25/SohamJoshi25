be12655380de9bbb85a4b0999e4e1798fd4a0343
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-07-09 18:45:48 +0530

    Resume Link  UPdate

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,5 +2,5 @@ export const Breakpoint = 660;
 export const Email = "contact@sohamjoshi.in";
 export const Github = "https://github.com/SohamJoshi25";
 export const Linkedin = "https://www.linkedin.com/in/sohamjoshi25/utm_source=share&utm_campaign=portfolio&utm_content=profile";
-export const Resume= "https://drive.google.com/file/d/1VeJX6DZ0muWpoXmSh1J_6KAUc50WEtKy/view?usp=sharing";
+export const Resume= "https://drive.google.com/file/d/1pRzq5Hs9et5XpnKPRy24xm5baYaN1avn/view?usp=sharing";
 export const EmailJsURL = "https://api.emailjs.com/api/v1.0/email/send";
