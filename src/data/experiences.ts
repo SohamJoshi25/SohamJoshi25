7ced90ba09a9c32c5649c3724e3510ec0c621b78
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-09-09 22:03:08 +0530

    internship link utm

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -18,6 +18,6 @@ export const ExperienceData : ExperienceType[] = [
         tags:["Express","React","MongoDB","TailwindCSS"],
         isFinished:false,
         text:"I worked as a Full Stack Developer Intern at Creatosaurus.io, where I developed and optimized platform features using React, Node.js, Express, and MongoDB. I integrated APIs to enhance functionality and implemented Deep Links to improve user navigation and interaction. My contributions focused on creating a seamless user experience while ensuring efficient communication between the frontend and backend.",
-        onClickLink:"https://www.app.creatosaurus.io/apps"
+        onClickLink:"https://www.app.creatosaurus.io/apps?utm_source=sohamjoshi.in&utm_medium=portfolio"
     }
 ];
